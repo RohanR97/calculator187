ff810e3b1833d6573f53f6901c7feebb6acbe8f7
rohan <rohan@DESKTOP-10OLR58>
2025-06-24 10:09:10 +0530

    added new files

---
+++ GitCollab/src/main/java/com/collab/GitCollab/GitCollabApplication.java
@@ -0,0 +1,13 @@
+package com.collab.GitCollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCollabApplication.class, args);
+	}
+
+}
---
+++ GitCollab/src/test/java/com/collab/GitCollab/GitCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.GitCollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
